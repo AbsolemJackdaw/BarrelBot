f0111aebc58daacf36ae0e5c0ce476121692bc33
bl4ckscor3 <donalddani9612@gmx.de>
2014-06-13 00:10:10 +0200

    fix

--- BarrelBot/BarrelBot/src/net/subaraki/commands/Commands.java
+++ BarrelBot/BarrelBot/src/net/subaraki/commands/Commands.java
@@ -5,6 +5,6 @@ import org.pircbotx.hooks.events.MessageEvent;
 @SuppressWarnings("rawtypes")
 public interface Commands <E extends MessageEvent>{
 
-	public void exe(MessageEvent event) throws Exception;
+	public void exe(E event) throws Exception;
 	public String getAlias();
 }
